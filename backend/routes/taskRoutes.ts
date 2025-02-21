5a2cec6685b0942e58fd7a8ca8c859110e7d5060
Ewiz <erizjsub2@gmail.com>
2025-02-20 19:07:48 -0800

    feat: added authentication middleware to task routes

--- backend/routes/taskRoutes.ts
+++ backend/routes/taskRoutes.ts
@@ -1,14 +1,19 @@
 import express from "express";
 import { taskController } from "../controllers/taskController.js";
+import { checkAuth } from "../middleware/checkAuth.js";
 
 const router = express.Router();
 
-router.get("/", (req, res, next) => taskController.getTasks(req, res, next));
-router.post("/", (req, res, next) => taskController.createTask(req, res, next));
-router.put("/:id", (req, res, next) =>
+router.get("/", checkAuth, (req, res, next) =>
+  taskController.getTasks(req, res, next)
+);
+router.post("/", checkAuth, (req, res, next) =>
+  taskController.createTask(req, res, next)
+);
+router.put("/:id", checkAuth, (req, res, next) =>
   taskController.updateTask(req, res, next)
 );
-router.delete("/:id", (req, res, next) =>
+router.delete("/:id", checkAuth, (req, res, next) =>
   taskController.deleteTask(req, res, next)
 );
 
